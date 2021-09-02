cfd0e4cc8b5313432eaa49cdb7766f780a23042c
zach <ZCroninDev@users.noreply.github.com>
2021-09-01 20:25:41 -0400

    Clear btn WIP

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ const taskInput = document.querySelector("#task");
 // Load all event listeners
 loadEventListeners();
 
+// Load all event listeners
 function loadEventListeners() {
   // Add task event
   form.addEventListener("submit", addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
 }
 
 // Add Task
@@ -29,7 +34,7 @@ function addTask(e) {
   // Add class
   link.className = 'delete-item secondary-content';
   // Add icon html
-  link.innnerHTML = '<i class="fa fa-remove"></i>';
+  link.innnerHTML = '<i class="fa fa-remove"></i>"';
   // Append the link to li
   li.appendChild(link);
 
@@ -42,3 +47,20 @@ function addTask(e) {
   e.preventDefault();
 }
 
+// Remove Task 
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains
+    ('delete-item')) {
+      if(confirm('Are you sure?')) {
+        e.target.parentElement.parentElement.remove();
+      }
+  }
+}
+
+
+// Clear Tasks 
+function clearTasks() {
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
